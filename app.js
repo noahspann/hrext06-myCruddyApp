e3cd7abada87e76ae30afcc54f462a6f2dbaf1e8
Johnathan Spann <johnathanspann@johnathans-mbp.attlocal.net>
2018-12-18 17:09:15 -0600

    finished basics

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 $(document).ready(function(){
-  console.log('jQuery loaded');
+  var valuesArr = [];
+  
+  var liMaker = function(input){
+    let $li = document.createElement('li');
+    $('ul').append('<li><label><input type="checkbox">'+ input +'</label></li>');
+  };
+  
+  localStorage.setItem('inputFieldValue', JSON.stringify(valuesArr));
+  var myItemInStorage = JSON.parse(localStorage.getItem('inputFieldValue'));
 
   // write to local storage from input when button save clicked
   $('.btn-submit').on('click', function(){
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
-    var myItemInStorage = localStorage.getItem('inputFieldValue');
-    console.log('myItemInStorage', myItemInStorage);
-
-    // display the value here
-    $('.list-display-field').text(myItemInStorage); // ??
-
+    valuesArr.push($('input').val());
+    localStorage.setItem('inputFieldValue', JSON.stringify(valuesArr));
+    liMaker($('input').val());
+    $('input').val('');
   });
 
-  // delete from local storage when delete button clicked
+  //delete from local storage when delete button clicked
   $('.btn-delete').on('click', function(){
-    localStorage.removeItem('inputFieldValue');
+    var removedVal = valuesArr.shift();
+    localStorage.setItem('inputFieldValue', JSON.stringify(valuesArr));
+    valuesArr = JSON.parse(localStorage.getItem('inputFieldValue'));
+    $('ul').html('');
+    valuesArr.forEach(item => {
+       $('ul').append('<li><label><input type="checkbox">'+ item +'</label></li>');
+    });
+   
+  });
+  //clear all local storage and refresh web page
+  $('.clear-all').on('click', () => {
+    localStorage.clear();
+    location.reload();
   });
 
 });
